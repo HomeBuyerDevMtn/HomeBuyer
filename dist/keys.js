f68bd0ce3eaee53a03d257b78c118c552d472c0f
heathermhargreaves <heathermhargreaves@gmail.com>
2016-09-05 12:52:21 -0600

    testing if keys show in dist file

--- dist/keys.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-  AWS: {
-    ACCESS_KEY: "AKIAIYHEHQKXFTOPK2IA",
-    ACCESS_SECRET: "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",
-    REGION: 'us-west-2'
-  }
-};
