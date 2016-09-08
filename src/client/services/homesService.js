022cc9ff0d976a89f2302c3d391b704d620aa8d2
Brandon Shepherd <brandonkshepherd@gmail.com>
2016-09-08 10:13:38 -0600

    added a email and password form

--- src/client/services/homesService.js
+++ src/client/services/homesService.js
@@ -1,5 +1,6 @@
 angular.module('homeBuyer').service('homeService', function($http){
-let baseurl = 'http://localhost:3000';
+// let baseurl = 'http://localhost:3000';
+let baseurl = 'http://192.168.0.108:3000';
   this.createHome = function(home){
     console.log(home);
     return $http({
