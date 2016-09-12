88cd9920b79470313bcffc567cf1f22135e192af
Brandon Shepherd <brandonkshepherd@gmail.com>
2016-09-12 09:41:46 -0600

    home view with funcitoning rate button

--- src/client/controllers/homeCtrl.js
+++ src/client/controllers/homeCtrl.js
@@ -43,12 +43,42 @@
     'src' : 'img/house3.jpg'
   }, {
     'src' : 'img/house4.jpg'
-	}];
+	},{
+		'src' : 'img/house.jpg'
+	}, {
+		'src' : 'img/house2.jpeg'
+	}, {
+		'src' : 'img/house3.jpg'
+  }, {
+    'src' : 'img/house3.jpg'
+  }, {
+    'src' : 'img/house3.jpg'
+  }, {
+    'src' : 'img/house4.jpg'
+	}, {
+		'src' : 'img/house.jpg'
+	}, {
+		'src' : 'img/house2.jpeg'
+	}, {
+		'src' : 'img/house3.jpg'
+  }, {
+    'src' : 'img/house3.jpg'
+  }, {
+    'src' : 'img/house3.jpg'
+  }, {
+    'src' : 'img/house4.jpg'
+	}
+
+];
 
 	$scope.showImages = function(index) {
 		$scope.activeSlide = index;
 		$scope.showModal('./views/image-modal.html');
 	}
+
+  $scope.showPriorities = function() {
+    $scope.showModal('./views/prioritiesTempl.html');
+  }
 
 	$scope.showModal = function(templateUrl) {
 		$ionicModal.fromTemplateUrl(templateUrl, {
@@ -65,82 +95,4 @@
 		$scope.modal.hide();
 		$scope.modal.remove()
 	};
-
-
-  // $scope.aImages = [{
-  //             'src' : './img/house.jpg',
-  //             'msg' : ''
-  //           }, {
-  //             'src' : './img/house2.jpeg',
-  //             'msg' : ''
-  //           }, {
-  //             'src' : './img/house3.jpg',
-  //             'msg' : ''
-  //         }, {
-  //             'src' : './img/house4.jpg',
-  //             'msg' : ''
-  //           }, {
-  //               'src' : './img/house4.jpg',
-  //               'msg' : ''
-  //             }, {
-  //                 'src' : './img/house4.jpg',
-  //                 'msg' : ''
-  //               }, {
-  //                   'src' : './img/house4.jpg',
-  //                   'msg' : ''
-  //                 }, {
-  //                     'src' : './img/house4.jpg',
-  //                     'msg' : ''
-  //         }];
-  // //
-  //         $ionicModal.fromTemplateUrl('./views/image-modal.html', {
-  //           scope: $scope,
-  //           animation: 'slide-in-up'
-  //         }).then(function(modal) {
-  //           $scope.modal = modal;
-  //         });
-  // //
-  //         $scope.openModal = function() {
-  //           $ionicSlideBoxDelegate.slide(0);
-  //           $scope.modal.show();
-  //         };
-  //
-  //         $scope.closeModal = function() {
-  //           $scope.modal.hide();
-  //         };
-  // //
-  //         // Cleanup the modal when we're done with it!
-  //         $scope.$on('$destroy', function() {
-  //           $scope.modal.remove();
-  //         });
-  //         // Execute action on hide modal
-  //         $scope.$on('modal.hide', function() {
-  //           // Execute action
-  //         });
-  //         // Execute action on remove modal
-  //         $scope.$on('modal.removed', function() {
-  //           // Execute action
-  //         });
-  //         $scope.$on('modal.shown', function() {
-  //           console.log('Modal is shown!');
-  //         });
-  // //
-  //         // Call this functions if you need to manually control the slides
-  //         $scope.next = function() {
-  //           $ionicSlideBoxDelegate.next();
-  //         };
-  //
-  //         $scope.previous = function() {
-  //           $ionicSlideBoxDelegate.previous();
-  //         };
-  //
-  //         $scope.goToSlide = function(index) {
-  //           $scope.modal.show();
-  //           $ionicSlideBoxDelegate.slide(index);
-  //         }
-  // //
-  //         // Called each time the slide changes
-  //         $scope.slideChanged = function(index) {
-  //           $scope.slideIndex = index;
-  //         };
-  });
+});
