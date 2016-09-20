ba3f5c0fe5419b82fc473f1645afd47e594d7a4a
Daniel Wood <wooddan22@gmail.com>
2016-09-19 22:45:55 -0600

    fixed query

--- db/read_homes_by_list_id.sql
+++ db/read_homes_by_list_id.sql
@@ -1,4 +1,4 @@
-select h.id, h.list_id, h.nickname, h.price, h.address_1, h.address_2, city, zip, province, bathrooms, bedrooms, h.sq_feet, h.year_build, h.description, h.days_listed, i.url, count(h.id) as home_count
+select h.id, h.list_id, h.nickname, h.price, h.address_1, h.address_2, city, zip, province, bathrooms, bedrooms, h.sq_feet, h.year_build, h.description, h.days_listed, i.url
 from homes h
 left join images i on h.cover_image_id = i.id where h.list_id = $1 and h.active = true;
 
